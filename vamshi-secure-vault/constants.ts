8a21cd444bce6c6ca7a8da856dcc5872e4e37015
Vamshi160 <rvamshik2005@gmail.com>
2025-08-01 19:02:16 +0530

    Delete vamshi-secure-vault directory

--- vamshi-secure-vault/constants.ts
+++
@@ -1,8 +0,0 @@
-import type { Category, Usecase } from './types';
-
-export const CATEGORIES: Category[] = ['website', 'email', 'username'];
-export const USECASES: Usecase[] = ['Default', 'Private', 'Gaming'];
-
-export const LOCAL_STORAGE_PIN_KEY = 'secureVaultPin';
-export const LOCAL_STORAGE_ENTRIES_KEY = 'secureVaultEntries';
-export const LOCAL_STORAGE_THEME_KEY = 'theme';
